5faaee62bbc5c1cd94a40df91e6bf0a479a26455
cyyworld71@gmail.com <yjjung@tecace.com>
2024-10-31 12:15:48 +0900

    delete

--- routes/lp.js
+++ routes/lp.js
@@ -56,7 +56,28 @@ router.post('/getAllLpList', async function(req, res){
 		res.status(403).send({message:q_res.errorMessage})
 	}
 })
+router.post('/updateById', async function(req, res){
+	let body = req.body
+	let q = `UPDATE  lp_list SET 
+	name=${body.name}, 
+	singer=${body.singer},
+	releaseDate=${body.releaseDate},
+	description=${body.description},
+	price=${body.price},
+	genre=${body.genre},
+	imgList=${JSON.stringify(body.imgList)},
+	WHERE lpId='${body.lpId}'`
 
+	console.log(q)
+	var q_res = await sql(q)
+	if(q_res.success){
+		console.log("success!")
+		res.status(200).json({data:body})
+	}else{
+		console.log("fail!")
+		res.status(403).send({message:q_res.errorMessage})
+	}
+})
 router.post('/deleteById', async function(req, res){
 	let body = req.body
 	if(body.imgList.length){
@@ -70,6 +91,25 @@ router.post('/deleteById', async function(req, res){
 		}
 	}
 	let q_res = await sql(`DELETE FROM lp_list WHERE lpId='${body.lpId}'`)
+	if(q_res.success){
+		res.status(200).json({data:q_res.data})
+	}else{
+		res.status(403).send({message:q_res.errorMessage})
+	}
+})
+router.post('/deleteImgById', async function(req, res){
+	let body = req.body
+	if(body.imgList.length){
+		for(var i in body.imgList){
+			let filePath = body.imgList[i].url.replace(SITE_URL,'.')
+			try {
+				fs.unlinkSync(filePath)
+			  } catch (err) {
+				console.error(err)
+			  }
+		}
+	}
+	let q_res = await sql(`UPDATE lp_list SET imgList = ${JSON.stringify([])} WHERE lpId='${body.lpId}'`)
 	if(q_res.success){
 		res.status(200).json({data:q_res.data})
 	}else{
